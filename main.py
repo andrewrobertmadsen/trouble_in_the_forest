5995f1796e684821ea4f74bf69ebe6a4fe13e2a9
andrew-madsen <andrewrobertmadsen@gmail.com>
2020-10-20 04:07:13 -0600

    Candidate solution using duckdb.

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-# This is my project
+""" A project to test duckdb."""
 
-#importing data from a csv into a list of dictionaries
-import csv
-import pprint
-with open("appt_data.csv", "r") as f:
-    reader = csv.DictReader(f)
-    appt_data = list(reader)
-    pprint.pprint(appt_data)
+import duckdb
 
-with open("pair_match.csv", "r") as f:
-    reader = csv.DictReader(f)
-    pairs = list(reader)
-    pprint.pprint(pairs)
+cursor = duckdb.connect()
+
+# Load data from csvs.
+cursor.execute("CREATE TABLE appointment AS SELECT * FROM read_csv_auto('appt_data.csv');")
+cursor.execute("CREATE TABLE cpt_pairs AS SELECT * FROM read_csv_auto('pair_match.csv');")
+
+# Match on the first cpt code, write to temp table.
+cursor.execute("""
+    CREATE TABLE cpt_match_first AS SELECT
+    patient_id
+    , appointment_id
+    , appointment_date
+    , cpt_code
+    , Pair_item1
+    , Pair_item2
+    , "Pair ID"
+    FROM appointment INNER JOIN cpt_pairs
+    ON appointment.cpt_code = cpt_pairs.Pair_item1
+    ;""")
+
+# Match second cpt code from temp table.
+cursor.execute("""
+    CREATE TABLE cpt_match_both AS SELECT
+    a.patient_id,
+    a.appointment_id,
+    c."Pair ID"
+    FROM appointment a INNER JOIN cpt_match_first c
+    ON a.cpt_code = c.Pair_item2
+    AND a.patient_id = c.patient_id
+    AND a.appointment_id = c.appointment_id
+    ;""")
+
+# Write matches to csv.
+cursor.execute("""
+    COPY cpt_match_both
+    TO 'cpt_matches.csv'
+    WITH (HEADER 1, DELIMITER ',')
+    ;""")
