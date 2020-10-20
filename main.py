f47ecf9bf3c90ec23c0d02e96bc36caebad8b0f9
Adam Mccoy <adammccoy@Adams-MacBook-Pro-2.local>
2020-10-19 23:06:46 -0600

    add functionality to read in .csv

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # This is my project
 
+#importing data from a csv into a list of dictionaries
+import csv
+import pprint
+with open("appt_data.csv", "r") as f:
+    reader = csv.DictReader(f)
+    appt_data = list(reader)
+    pprint.pprint(appt_data)
 
+with open("pair_match.csv", "r") as f:
+    reader = csv.DictReader(f)
+    pairs = list(reader)
+    pprint.pprint(pairs)
